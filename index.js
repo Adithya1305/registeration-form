6e821fb056b1f86232235004f09fe30777613cdd
Adithya1305 <132258530+Adithya1305@users.noreply.github.com>
2024-08-19 19:55:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require("express")
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+const port = process.env.port || 3000;
+
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.aeu8l.mongodb.net/registerationFormDB`, {
+  useNewUrlParser : true,
+  useUnifiedTopology : true,
+});
+// registeration schema
+const registerationSchema = new mongoose.Schema({
+  name : String,
+  email : String,
+  password : String,
+})
+
+// mode of registration schema
+const Registeration = mongoose.model("Registeration",registerationSchema);
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+app.get("/",(req,res) =>{
+  res.sendFile(__dirname + "/pages/index.html");
+})
+
+app.post("/register", async (req,res) =>{
+  try{
+    const {name, email,password} = req.body;
+
+    const existingUser = await Registeration.findOne({email : email});
+    //check for existing user
+    if(!existingUser){
+      const registerationData = new Registeration({
+        name,
+        email,
+        password
+      });
+      await registerationData.save();
+      res.redirect("/success");
+    }
+    else{
+      console.log("User already exist");
+      res.redirect("/error");
+    }
+     
+  }
+  catch (error){
+    console.log(error);
+    res.redirect("error");
+  }
+});
+
+
+app.get("/success",(req,res)=>{
+  res.sendFile (__dirname+"/pages/success.html");
+})
+app.get("/error",(req,res)=>{
+  res.sendFile (__dirname+"/pages/error.html");
+})
+
+
+app.listen(port,()=>{
+  console.log(`server is running on port ${port}`);
+})
